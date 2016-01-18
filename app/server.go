0a61be16167dbe0956084e169214fedc138384a1
Alexander Svensson <gurkslask@gmail.com>
2016-01-18 16:01:00 +0100

    End of the day save

--- app/server.go
+++ app/server.go
@@ -2,26 +2,23 @@ package main
 
 import (
 	"html/template"
-	"io/ioutil"
 	"net/http"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
+type Page interface {
+	Title string,
+	tmpl string
 }
 
-func loadPage(title string) (*Page, error) {
-	filename := "static/" + title + ".html"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
+	t, _ := template.ParseFiles("static/" + p.tmpl + ".html")
+	t.Execute(w, r)
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("static/home.html")
+	test := "hejsan svejsan"
+	if test == "a" {
+	}
 	t.Execute(w, r)
 }
 
