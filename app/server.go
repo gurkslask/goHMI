67466334deb1929e3b3c1314cbd9129ff49393d7
Alexander Svensson <gurkslask@gmail.com>
2016-01-19 10:55:52 +0100

    This will go away

--- app/server.go
+++ app/server.go
@@ -5,21 +5,10 @@ import (
 	"net/http"
 )
 
-type Page interface {
-	Title string,
-	tmpl string
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
-	t, _ := template.ParseFiles("static/" + p.tmpl + ".html")
-	t.Execute(w, r)
-
 func handler(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("static/home.html")
 	test := "hejsan svejsan"
-	if test == "a" {
-	}
-	t.Execute(w, r)
+	t.Execute(w, test)
 }
 
 func main() {
